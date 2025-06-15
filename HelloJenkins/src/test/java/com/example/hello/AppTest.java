3dfb4d6e961ae1524878b6777125a2b5bd389b66
Moinuddin-31 <2021wb86300@wilp.bits-pilani.ac.in>
2025-06-15 15:13:20 +0530

    Update AppTest.java

--- HelloJenkins/src/test/java/com/example/hello/AppTest.java
+++ HelloJenkins/src/test/java/com/example/hello/AppTest.java
@@ -1,38 +1,13 @@
 package com.example.hello;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+ 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+ 
+import org.junit.jupiter.api.Test;
+ 
+public class AppTest {
+ 
+@Test
+public void shouldAnswerWithTrue() {
+    assertTrue(true);
+}
 }
